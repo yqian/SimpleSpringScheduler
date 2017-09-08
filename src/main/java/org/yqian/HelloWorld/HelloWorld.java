6d73fbe413df6fe5de9de649869726df2808b82d
yqian <yi.qian@att.net>
2017-09-08 17:04:23 -0400

    Create HelloWorld.java

---
+++ src/main/java/org/yqian/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.yqian.HelloWorld;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Hello World at " + new Date());
+}
+}
