77d8b83dc9f8ac018b6a74de26c15036d76635f2
Yi Qian <yqian@localhost.localdomain>
2017-09-10 20:43:37 -0400

    update parent pom

--- src/main/java/org/yqian/HelloWorldSchedulerParent/HelloWorldWebScheduler/HelloWorldSchedulerListenerServlet.java
+++
@@ -1,7 +0,0 @@
-package org.yqian.HelloWorldWebScheduler;
-import javax.servlet.ServletContextEvent;import javax.servlet.ServletContextListener;import javax.servlet.annotation.WebListener;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-@WebListenerpublic class HelloWorldSchedulerServlet implements ServletContextListener {
-private AnnotationConfigApplicationContext applicationContext = null;
-public void contextDestroyed(ServletContextEvent arg0) {applicationContext.close();}
-public void contextInitialized(ServletContextEvent arg0) {applicationContext = new AnnotationConfigApplicationContext(HelloWorldScheduler.class);}}
